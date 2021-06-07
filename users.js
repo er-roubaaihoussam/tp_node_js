02b2a7b3da16de1657ac7ac9eaa87412937fd6f3
er-roubaaihoussam <houssamerroubaai10@gmail.com>
2021-06-07 15:41:31 +0200

    q15 on complete l'implementation du fichier users.js

--- users.js
+++ users.js
@@ -4,14 +4,14 @@ module.exports = {
     return User.findAll()
     },
     // méthodes à implémenter
-    getUsers(offset = 0, limit = 10) { },
-    getAdmins() { },
-    getAuthors() { },
-    getGuests(){ },
-    getUser(id) { },
-    getUserByEmail(email) { },
-    addUser(user) { },
-    updateUser() { },
-    deleteUser() { },
+    getUsers(offset = 0, limit = 10) { return User.getUsers(offset,limit)},
+    getAdmins() {return User.getAdmins() },
+    getAuthors() { return User.getAuthors()},
+    getGuests(){return User.getGuests() },
+    getUser(id) {return User.getUser(id)},
+    getUserByEmail(email) { return User.getUserByEmail()},
+    addUser(user) { return User.addUser(user)},
+    updateUser() { return User.updateUser()},
+    deleteUser() { return User.deleteUser()},
     // D'autres méthodes jugées utiles
     }
