8cdd35c16425c8929735d3beb4585675695f94f4
er-roubaaihoussam <houssamerroubaai10@gmail.com>
2021-06-07 18:22:42 +0200

    q16 on ajoute le code le code permettant de répondre aux routes

--- users.js
+++ users.js
@@ -1,20 +1,40 @@
-const { User } = require('../models')
-module.exports = {
-    getAllUsers() {
-    return User.findAll()
-    },
-    // méthodes à implémenter
-    getUsers(offset = 0, limit = 10) { return User.findAll({ offset: offset, limit: limit })},
-    getAdmins() {return User.findAll({where: {role:'admin'}}) },
-    getAuthors() { return User.findAll({where: {role:'author'}}) },
-    getGuests(){return User.findAll({where: {role:'guest'}}) },
-    getUser(id) {return User.findAll({where: {id:id}})},
-    getUserByEmail(email) { return User.findAll({where: {email:email}})},
-    addUser(user) {  User.create(user)},
-    updateUser(user) {  User.update(user, {})},
-    deleteUser(id) {  User.destroy({where: { id: id }})},
-    
+var express = require('express');
+const { Model } = require('sequelize/types');
+var router = express.Router();
+const usersRepo = require('../respositories/users')
+
+/* GET users listing. */
+router.get('/', async function(req, res, next) {
+  res.send(await usersRepo.getAllUsers());
+});
+
+router.get('/users',async function(req, res, next) {
+  res.send(await usersRepo.getUsers());
+  next()
+});
+
+router.get('/users/:id',async function(req, res, next) {
+  res.send(await usersRepo.getUser(req.params.id));
+  next()
+});
+
+router.post('/users',async function(req, res, next) {
+  res.send( usersRepo.addUser(req.body));
+  next()
+});
+
+router.put('/users',async function(req, res, next) {
+  res.send( usersRepo.updateUser(req.body));
+  next()
+});
+
+router.delete('/users:/id',async function(req, res, next) {
+  res.send( usersRepo.deleteUser(req.params.id));
+  next()
+});
 
 
-    // D'autres méthodes jugées utiles
-    }
+
+
+
+module.exports = router;
